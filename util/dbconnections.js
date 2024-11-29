00d0d1bb2adeaf83ff87dcc709c8e58575e136ce
Calvin <calvinsidik@gmail.com>
2024-11-29 20:22:59 +0700

    fixing db cld con

--- util/dbconnections.js
+++ util/dbconnections.js
@@ -1,43 +1,43 @@
 const { Pool } = require("pg");
-// const { Signer } = require('@aws-sdk/rds-signer')
+const { Signer } = require('@aws-sdk/rds-signer')
 
 
-// const signerOptions = {
-//   credentials: {
-//     accessKeyId: process.env.AWSACCESSKEY,
-//     secretAccessKey: process.env.AWSSECRETKEY
-//   },
-//   region: process.env.RDSDBREGION,
-//   hostname: process.env.RDSHOSTNAME,
-//   port: process.env.RDSDBPORT,
-//   username: process.env.RDSDBUSERNAME
-// }
+const signerOptions = {
+  credentials: {
+    accessKeyId: process.env.AWSACCESSKEY,
+    secretAccessKey: process.env.AWSSECRETKEY
+  },
+  region: process.env.RDSDBREGION,
+  hostname: process.env.RDSHOSTNAME,
+  port: process.env.RDSDBPORT,
+  username: process.env.RDSDBUSERNAME
+}
 
-// const signer = new Signer()
+const signer = new Signer()
 
-// const getPassword = async () => await signer.getAuthToken(signerOptions)
-
-
+const getPassword = async () => await signer.getAuthToken(signerOptions)
 
 
-// const pool = new Pool({
-//   user: signerOptions.username,
-//   password: process.env.RDSPASSWORD,
-//   host: signerOptions.hostname,
-//   port: signerOptions.port,
-//   database: process.env.RDSDBNAME,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// })
+
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "pandoorbox",
-  password: "!V1nzt3r91",
-  port: 5432,
-});
+  user: signerOptions.username,
+  password: process.env.RDSPASSWORD,
+  host: signerOptions.hostname,
+  port: signerOptions.port,
+  database: process.env.RDSDBNAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "pandoorbox",
+//   password: "!V1nzt3r91",
+//   port: 5432,
+// });
 
 module.exports = {
   pool,
--- validations.js
+++ validations.js
@@ -241,6 +241,7 @@ exports.requestNewPaymentValidation = function(data){
     payment_method_id: Joi.number().min(1).max(999999999).positive().integer().required(),
     payment_number: Joi.string().min(1).max(256).required(),
     payment_link: Joi.string().min(1).max(256).required(),
+    payment_vendor: Joi.string().min(1).max(256).required()
   })
   return schema.validate(data)
 }
