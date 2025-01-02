08d68a1a5653be0092bccf63bad2078b2978f8b7
Calvin <calvinsidik@gmail.com>
2025-01-03 00:23:55 +0700

    trx user valid

--- controllers/paymentRequestController.js
+++ controllers/paymentRequestController.js
@@ -331,7 +331,7 @@ async function generateQRISE2Pay(dataBody) {
   console.log(signature);
   console.log(process.env.MERCHANTKEY);
 
-  const user = db.pool.query({ text: "SELECT * FROM user WHERE $1", values: [dataBody.user_id] })
+  const user = await db.pool.query({ text: "SELECT * FROM user WHERE $1", values: [dataBody.user_id] })
 
   console.log(user);
 
