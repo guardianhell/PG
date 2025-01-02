1e4ef081e82b461ec8ee7f513ac5b4ec6f41e8dc
Calvin <calvinsidik@gmail.com>
2025-01-02 23:37:58 +0700

    trx user valid

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -3,7 +3,7 @@ const transactionController = require("../controllers/transactionController");
 const paymentRequestController = require("../controllers/paymentRequestController");
 const verify = require("../verify");
 
-router.post("/create", verify, transactionController.createNewTransaction);
+router.post("/create", transactionController.createNewTransaction);
 router.get("/", async function (req, res) {
   return res.send("HAI");
 });
