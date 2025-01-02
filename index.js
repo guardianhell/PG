607beabdbc4c9df665c59f646ecf750c241ccec3
Calvin <calvinsidik@gmail.com>
2025-01-02 23:49:04 +0700

    trx user valid

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const paymentTypeRoute = require("./routes/paymentTypeRoute");
 const paymentRequestRoute = require("./routes/paymentRequestRoute");
 const callbackRoute = require("./routes/callbackRoute")
 
+const corsConfig = {
+  credentials: true,
+  origin: true,
+};
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -27,7 +32,17 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
-app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(cors(corsConfig))
 
 app.use(urlAPI + "/unit", unitRoute);
 app.use(urlAPI + "/product-category", productCategoryRoute);
