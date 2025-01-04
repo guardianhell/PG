17237c29d4eb37896f742481cde61c9796d719d3
Calvin <calvinsidik@gmail.com>
2025-01-04 12:22:02 +0700

    building repaymentRequest

--- routes/paymentRequestRoute.js
+++ routes/paymentRequestRoute.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const paymentRequestController = require("../controllers/paymentRequestController");
 
 router.post("/request", paymentRequestController.requestNewPayment);
+router.post("/rePaymentRequest", paymentRequestController.repaymentRequest)
 
 module.exports = router;
