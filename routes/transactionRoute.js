e858309642994da6f1dbffc55dcfbac903d2f429
Calvin <calvinsidik@gmail.com>
2025-01-11 17:05:56 +0700

    trying new Method Create TRX

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -3,7 +3,7 @@ const transactionController = require("../controllers/transactionController");
 const paymentRequestController = require("../controllers/paymentRequestController");
 const verify = require("../verify");
 
-router.post("/create", verify, transactionController.createNewTransaction);
+router.post("/create", verify, transactionController.createNewTransaction2);
 router.get("/transactionList", verify, transactionController.getTransactionByUserId)
 
 
